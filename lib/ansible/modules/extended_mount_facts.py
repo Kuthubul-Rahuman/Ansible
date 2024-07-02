1dab43bf07b8aa108446f58ed2b8c06eabb1f4fb
s-hertel <19572925+s-hertel@users.noreply.github.com>
2024-07-02 09:31:21 -0400

    Fix using no timeout, and make timeout required to fix ambiguous expectations
    Instead of hardcoding fstype choices, support UNIX shell wildcards
    
    Allow filtering on fstype and the device
    
    Use timeout when calling mount
    
    Check sys.platform before using mount regex

--- lib/ansible/modules/extended_mount_facts.py
+++ lib/ansible/modules/extended_mount_facts.py
@@ -9,29 +9,26 @@ DOCUMENTATION = """
 ---
 module: extended_mount_facts
 version_added: 2.18
-short_description: Get mount facts for devices that do not start with a path separator.
+short_description: Retrieve information about mounts that match both a requested file system in O(fstypes) and device in O(devices).
 description:
-  - Get mount facts for devices that do not start with a path separator, which are skipped by default mount gathering.
+  - Retrieve information about mounts that match both a requested file system in O(fstypes) and device in O(devices).
 options:
-  collect:
-    description: A list of mount types to return.
+  devices:
+    description: A list of fnmatch patterns to filter mounts by the special device or remote file system.
+    required: true
     type: list
     elements: str
+  fstypes:
+    description: A list of fnmatch patterns to filter mounts by the type of the file system.
     required: true
-    choices:
-      - cifs
-      - ceph
-      - fuse
-      - glusterfs
-      - gpfs
-      - nfs
-      - nfs4
-      - vboxsf
-      - zfs
+    type: list
+    elements: str
   timeout:
-    description: The maximum number of seconds to query for each mount.
-    default: 20
-    type: int
+    description:
+      - This is the maximum number of seconds to query for each mount. This can also be set to V(null) to wait indefinitely.
+      - This timeout also applies to the C(mount) command, which is executed if mounts cannot be listed using a file.
+    required: true
+    type: raw
   on_timeout:
     description:
       - The action to take when a timeout occurs.
@@ -43,14 +40,14 @@ options:
     default: error
     type: str
 extends_documentation_fragment:
-    - action_common_attributes
+  - action_common_attributes
 attributes:
-    check_mode:
-        support: full
-    diff_mode:
-        support: none
-    platform:
-        platforms: posix
+  check_mode:
+    support: full
+  diff_mode:
+    support: none
+  platform:
+    platforms: posix
 author:
   - Ansible Core Team
 """
@@ -58,9 +55,12 @@ author:
 EXAMPLES = """
 - name: Get NFS and FUSE subtype mounts
   extended_mount_facts:
-    collect:
-      - nfs
-      - fuse
+    devices:
+      - "[!/]*"
+    fstypes:
+      - "nfs"
+      - "nfs4"
+      - "fuse.*"
 """
 
 RETURN = """
@@ -88,13 +88,17 @@ ansible_facts:
 """
 
 from ansible.module_utils.basic import AnsibleModule
+from ansible.module_utils.common.validation import check_type_int
 from ansible.module_utils.facts import timeout
 from ansible.module_utils.facts.hardware.linux import LinuxHardware
 from ansible.module_utils.facts.utils import get_mount_size, get_file_content
 
+from fnmatch import fnmatch
 from typing import Callable, Optional
+from sys import platform
 
 import re
+import subprocess
 
 # Regular expressions to match individual lines from mount
 
@@ -112,19 +116,6 @@ AIX_MOUNT_RE = re.compile(r"^(?P<node>\S+)\s+(?P<mount>\S+)\s+(?P<mounted_over>\
 # Solaris uses /etc/mnttab
 
 
-TYPES = [
-    "cifs",
-    "ceph",
-    "fuse",
-    "glusterfs",
-    "gpfs",
-    "nfs",
-    "nfs4",
-    "vboxsf",
-    "zfs",
-]
-
-
 def get_mount_info(
     module: AnsibleModule,
     mount: str,
@@ -133,24 +124,26 @@ def get_mount_info(
     udevadm_uuid: Callable[[str], str]
 ) -> tuple[Optional[dict], Optional[str]]:
     """
-    Attempts to get the mount size and UUID within the specified timeout.
+    Attempts to get the mount size and UUID within an optional timeout.
     """
-    seconds = module.params["timeout"]
-    on_error = module.params["on_timeout"]
-
-    @timeout.timeout(seconds)
     def _get_mount_info(mount, device, uuids, udevadm_uuid):
         mount_size = get_mount_size(mount)
         uuid = uuids.get(device, udevadm_uuid(device))
         return mount_size, uuid
 
-    try:
+    if module.params["timeout"] is None:
         return _get_mount_info(mount, device, uuids, udevadm_uuid)
+
+    seconds = module.params["timeout"]
+    on_error = module.params["on_timeout"]
+    try:
+        return timeout.timeout(seconds)(_get_mount_info)(mount, device, uuids, udevadm_uuid)
     except timeout.TimeoutError:
         if on_error == "error":
             module.fail_json(msg=f"{device} on {mount} timed out")
         elif on_error == "warn":
             module.warn(f"{device} on {mount} timed out")
+
     return None, None
 
 
@@ -158,7 +151,11 @@ def parse_mount(line: str) -> Optional[dict[str, str]]:
     """
     Parse a line of mount information and return a dictionary with mount details.
     """
-    match = BSD_MOUNT_RE.match(line) or AIX_MOUNT_RE.match(line)
+    match = None
+    if platform.startswith(("freebsd", "netbsd", "openbsd", "dragonfly", "darwin", "linux")):
+        match = BSD_MOUNT_RE.match(line)
+    if platform.startswith(("aix", "linux")):
+        match = match or AIX_MOUNT_RE.match(line)
     if match is None:
         return None
 
@@ -191,10 +188,23 @@ def parse_mount_output(module: AnsibleModule) -> list[dict[str, str]]:
     mount_path = module.get_bin_path("mount")
     mount_output = ""
     if mount_path:
-        rc, mount_out, err = module.run_command(mount_path, check_rc=True)
+        if platform.startswith("dragonfly"):
+            args = [mount_path, "-p"]
+        else:
+            args = [mount_path]
+        try:
+            mount_output = subprocess.run(*args, capture_output=True, text=True, check=True, timeout=module.params["timeout"])
+        except subprocess.TimeoutExpired as e:
+            if module.params["on_timeout"] == "error":
+                module.fail_json(msg="Running the mount command timed out, unable to retrieve mount facts.")
+            elif module.params["on_timeout"] == "warn":
+                module.warn("Running the mount command timed out, unable to retrieve mount facts.")
+            return []
+        except subprocess.CalledProcessError as e:
+            module.fail_json(msg=f"An error occurred while running the mount command: {str(e)}")
 
     mounts = []
-    for line in mount_out.splitlines():
+    for line in mount_output.splitlines():
         if (mount_info := parse_mount(line)):
             mounts.append(mount_info)
     return mounts
@@ -244,12 +254,21 @@ def list_current_mounts(module: AnsibleModule) -> list[dict[str, str]]:
 def main():
     module = AnsibleModule(
         argument_spec=dict(
-            collect=dict(type="list", elements="str", required=True, choices=TYPES),
-            timeout=dict(type="int", default=20),
+            devices=dict(type="list", elements="str", required=True),
+            fstypes=dict(type="list", elements="str", required=True),
+            timeout=dict(type="raw", required=True),
             on_timeout=dict(choices=["error", "warn", "ignore"], default="error"),
         ),
         supports_check_mode=True,
     )
+    if (timeout := module.params["timeout"]) is not None:
+        try:
+            module.params["timeout"] = check_type_int(timeout)
+        except TypeError as te:
+            module.fail_json(msg=f"argument 'timeout' is of type {type(timeout)} and we were unable to convert to int: {te}")
+        if module.params["timeout"] <= 0:
+            module.fail_json(msg="argument 'timeout' must be a positive integer or null")
+
     results = {}
 
     hardware = LinuxHardware(module)
@@ -261,12 +280,9 @@ def main():
         device = fields["device"]
         fstype = fields["fstype"]
 
-        if device.startswith("/"):
+        if not any(fnmatch(device, pattern) for pattern in module.params["devices"]):
             continue
-        if fstype.startswith(("fuse.", "fusefs.")):
-            if "fuse" not in module.params["collect"]:
-                continue
-        elif fstype not in module.params["collect"]:
+        if not any(fnmatch(fstype, pattern) for pattern in module.params["fstypes"]):
             continue
 
         mount_size, uuid = get_mount_info(module, mount, device, uuids, udevadm_uuid)
