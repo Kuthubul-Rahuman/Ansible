18bc35140405e0d534e3c17551ebf6f4a47c8d20
Brian Coca <brian.coca+git@gmail.com>
2024-08-06 13:37:03 -0400

    removed cipher specific tests
     made editor use rot13 cipher instead of requiring crypto lib

--- test/units/parsing/vault/test_vault_editor.py
+++ test/units/parsing/vault/test_vault_editor.py
@@ -36,16 +36,11 @@ from ansible.module_utils.common.text.converters import to_bytes, to_text
 
 from units.mock.vault_helper import TextVaultSecret
 
-v11_data = """$ANSIBLE_VAULT;1.1;AES256
-62303130653266653331306264616235333735323636616539316433666463323964623162386137
-3961616263373033353631316333623566303532663065310a393036623466376263393961326530
-64336561613965383835646464623865663966323464653236343638373165343863623638316664
-3631633031323837340a396530313963373030343933616133393566366137363761373930663833
-3739"""
+v11_data = """$ANSIBLE_VAULT;1.1;ROT13
+c2JiCg==
+"""
 
 
-@pytest.mark.skipif(not aes256.HAS_CRYPTOGRAPHY,
-                    reason="Skipping cryptography tests because cryptography is not installed")
 class TestVaultEditor(unittest.TestCase):
 
     def setUp(self):
@@ -92,7 +87,7 @@ class TestVaultEditor(unittest.TestCase):
     def _vault_editor(self, vault_secrets=None):
         if vault_secrets is None:
             vault_secrets = self._secrets(self.vault_password)
-        return VaultEditor(VaultLib(vault_secrets))
+        return VaultEditor(VaultLib(vault_secrets, cipher_name='rot13'))
 
     @patch('ansible.parsing.vault.subprocess.call')
     def test_edit_file_helper_empty_target(self, mock_sp_call):
